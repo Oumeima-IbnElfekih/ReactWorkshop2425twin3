08c56352efd9b4a079ac35fd67fa6b1dba06fdaa
Oumeima-IbnElfekih <oumeima.ibnelfekih@esprit.tn>
2025-02-24 10:13:33 +0100

    init router dom&dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@
 import { useState } from 'react';
 import './App.css'
 import Events from './UseCase/Events';
-
+import ColorBox from './ExercicesProps&State/ColorBox';
+import { Routes , Route } from 'react-router-dom';
+import Counter from './ExercicesProps&State/Counter';
 
 function App() {
   
 
   return (
     <>
-     <Events />
-       
+     {/* <Events /> */}
+     {/* <ColorBox initialColor="red"  /> */}
+     <Routes>
+      <Route path="/test" > 
+        <Route path="colorbox/:username/:classe" element={<ColorBox initialColor="red" />} />
+        <Route  path="counter" element={<Counter step="5" />} />
+      </Route>
+     </Routes>
     </>
   )
 }
--- src/ExercicesProps&State/ColorBox.jsx
+++ src/ExercicesProps&State/ColorBox.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
-
+import { useParams } from "react-router-dom";
 const ColorBox = (props) => {
+  const {username,classe} = useParams();
+  console.log(username);
   const [color, setColor] = useState(props.initialColor);
 
   const changeColor = () => {
@@ -10,7 +12,9 @@ const ColorBox = (props) => {
   };
 
   return (
-   <> <button onClick={changeColor}>Changer de couleur</button>
+   <> 
+   <h1>Valeur du param: {classe} {username}</h1>
+   <button onClick={changeColor}>Changer de couleur</button>
     <div style={{ width: "100px", height: "100px", backgroundColor: color, margin: "10px auto" }}>
       
     </div></>
