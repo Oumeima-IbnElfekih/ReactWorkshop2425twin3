561e2c49e819da0c692b4cff28b5f3e1456167b1
Oumeima-IbnElfekih <oumeima.ibnelfekih@esprit.tn>
2025-02-24 10:41:03 +0100

    lazy laoding

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
-import { useState } from 'react';
+import React,{ Suspense, useState } from 'react';
 import './App.css'
 import Events from './UseCase/Events';
-import ColorBox from './ExercicesProps&State/ColorBox';
-import { Routes , Route } from 'react-router-dom';
-import Counter from './ExercicesProps&State/Counter';
-
+// import ColorBox from './ExercicesProps&State/ColorBox';
+import { Routes , Route ,Link} from 'react-router-dom';
+// import Counter from './ExercicesProps&State/Counter';
+const ColorBox = React.lazy(()=>import('./ExercicesProps&State/ColorBox'));
+const Counter = React.lazy(()=>import('./ExercicesProps&State/Counter'));
 function App() {
   
 
@@ -13,12 +14,17 @@ function App() {
     <>
      {/* <Events /> */}
      {/* <ColorBox initialColor="red"  /> */}
+     {/* <a href='/test/colorbox/oumeima/twin'>Colobox with ancer</a> */}
+     <Link to='/test/colorbox/oumeima/twin' reloadDocument>Colobox with Link</Link>
+     <Suspense fallback={<p>Loading...</p>}>
      <Routes>
       <Route path="/test" > 
         <Route path="colorbox/:username/:classe" element={<ColorBox initialColor="red" />} />
         <Route  path="counter" element={<Counter step="5" />} />
       </Route>
+      <Route path="*" element={<p>404 not found</p>} />
      </Routes>
+     </Suspense>
     </>
   )
 }
